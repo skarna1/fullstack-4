cde564c5ac64a7d380840869d5b2e3aa76ac388d
Sami K <sami.karna@gmail.com>
2018-07-24 21:00:15 +0300

    Add tests for totalLikes in 4.4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,8 +1,15 @@
-const dummy = (blogs) => {
+const dummy = () => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  const reducer = (sum, blog) => {
+    return sum + blog.likes
+  }
+  return blogs.reduce(reducer, 0)
+}
+
 module.exports = {
-  dummy
+  dummy, totalLikes
 }
 
