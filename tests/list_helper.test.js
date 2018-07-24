7810fe54dd35de1578581d5d7fc06fcfb531c09f
Sami K <sami.karna@gmail.com>
2018-07-24 23:08:28 +0300

    Implement mostBlogs 4.6

--- tests/list_helper.test.js
+++ tests/list_helper.test.js
@@ -7,100 +7,143 @@ test('dummy is called', () => {
   expect(result).toBe(1)
 })
 
-describe('total likes', () => {
-  const listWithOneBlog = [
-    {
-      _id: '5a422aa71b54a676234d17f8',
-      title: 'Go To Statement Considered Harmful',
-      author: 'Edsger W. Dijkstra',
-      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-      likes: 5,
-      __v: 0
-    }
-  ]
+const listWithOneBlog = [
+  {
+    _id: '5a422aa71b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5,
+    __v: 0
+  }
+]
 
+const listWithOneBlogWithZeroLikes = [
+  {
+    _id: '5a422aa71b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 0,
+    __v: 0
+  }
+]
+
+const listWithZeroBlogs = []
+
+const listWithManyBlogs = [
+  {
+    _id: '5a422a851b54a676234d17f7',
+    title: 'React patterns',
+    author: 'Michael Chan',
+    url: 'https://reactpatterns.com/',
+    likes: 7,
+    __v: 0
+  },
+  {
+    _id: '5a422aa71b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5,
+    __v: 0
+  },
+  {
+    _id: '5a422b3a1b54a676234d17f9',
+    title: 'Canonical string reduction',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+    likes: 12,
+    __v: 0
+  },
+  {
+    _id: '5a422b891b54a676234d17fa',
+    title: 'First class tests',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
+    likes: 10,
+    __v: 0
+  },
+  {
+    _id: '5a422ba71b54a676234d17fb',
+    title: 'TDD harms architecture',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
+    likes: 0,
+    __v: 0
+  },
+  {
+    _id: '5a422bc61b54a676234d17fc',
+    title: 'Type wars',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
+    likes: 2,
+    __v: 0
+  }
+]
+
+describe('total likes', () => {
   test('when list has only one blog equals the likes of that', () => {
     const result = listHelper.totalLikes(listWithOneBlog)
     expect(result).toBe(5)
   })
-
-  const listWithOneBlogWithZeroLikes = [
-    {
-      _id: '5a422aa71b54a676234d17f8',
-      title: 'Go To Statement Considered Harmful',
-      author: 'Edsger W. Dijkstra',
-      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-      likes: 0,
-      __v: 0
-    }
-  ]
 
   test('when list has only one blog with 0 likes equals the likes of 0', () => {
     const result = listHelper.totalLikes(listWithOneBlogWithZeroLikes)
     expect(result).toBe(0)
   })
 
-  const listWithManyBlogs = [
-    {
-      _id: '5a422a851b54a676234d17f7',
-      title: 'React patterns',
-      author: 'Michael Chan',
-      url: 'https://reactpatterns.com/',
-      likes: 7,
-      __v: 0
-    },
-    {
-      _id: '5a422aa71b54a676234d17f8',
-      title: 'Go To Statement Considered Harmful',
-      author: 'Edsger W. Dijkstra',
-      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-      likes: 5,
-      __v: 0
-    },
-    {
-      _id: '5a422b3a1b54a676234d17f9',
-      title: 'Canonical string reduction',
-      author: 'Edsger W. Dijkstra',
-      url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
-      likes: 12,
-      __v: 0
-    },
-    {
-      _id: '5a422b891b54a676234d17fa',
-      title: 'First class tests',
-      author: 'Robert C. Martin',
-      url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
-      likes: 10,
-      __v: 0
-    },
-    {
-      _id: '5a422ba71b54a676234d17fb',
-      title: 'TDD harms architecture',
-      author: 'Robert C. Martin',
-      url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
-      likes: 0,
-      __v: 0
-    },
-    {
-      _id: '5a422bc61b54a676234d17fc',
-      title: 'Type wars',
-      author: 'Robert C. Martin',
-      url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
-      likes: 2,
-      __v: 0
-    }
-  ]
   test('when list has many blogs equal the sum of all likes', () => {
     const result = listHelper.totalLikes(listWithManyBlogs)
     expect(result).toBe(36)
   })
 
-
-
-  const listWithZeroBlogs = []
   test('when list has zero elements equal to 0', () => {
     const result = listHelper.totalLikes(listWithZeroBlogs)
     expect(result).toBe(0)
   })
 })
 
+describe('favorite blog', () => {
+
+  test('when list has only one blog equals the that blog', () => {
+    const result = listHelper.favoriteBlog(listWithOneBlog)
+    expect(result).toEqual(listWithOneBlog[0])
+  })
+
+  test('when list has many blogs equal the blog with most likes', () => {
+    const result = listHelper.favoriteBlog(listWithManyBlogs)
+    expect(result).toEqual(listWithManyBlogs[2])
+  })
+
+  test('when list has zero elements equal to null', () => {
+    const result = listHelper.favoriteBlog(listWithZeroBlogs)
+    expect(result).toBe(null)
+  })
+
+})
+
+describe('most blogs', () => {
+
+  test('when list has only one blog equals the that blog', () => {
+    const result = listHelper.mostBlogs(listWithOneBlog)
+    expect(result).toEqual({
+      author: 'Edsger W. Dijkstra',
+      blogs: 1
+    })
+  })
+
+  test('when list has many blogs equal the blog with most likes', () => {
+    const result = listHelper.mostBlogs(listWithManyBlogs)
+    expect(result).toEqual({
+      author: 'Robert C. Martin',
+      blogs: 3
+    })
+  })
+
+  test('when list has zero elements equal to null', () => {
+    const result = listHelper.mostBlogs(listWithZeroBlogs)
+    expect(result).toEqual({ 'author': undefined, 'blogs': undefined })
+  })
+
+})
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -9,7 +9,32 @@ const totalLikes = (blogs) => {
   return blogs.reduce(reducer, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  const reducer = (blogWithMaxLikes, blog) => {
+    return (blogWithMaxLikes.likes > blog.likes) ? blogWithMaxLikes : blog
+  }
+  return blogs.length === 0 ? null : blogs.reduce(reducer, blogs[0])
+}
+
+const mostBlogs = (blogs) => {
+  const countAuthors = (authors, blog) => {
+    authors[blog.author] ? authors[blog.author]++ : authors[blog.author] = 1
+    return authors
+  }
+
+  const authors = blogs.reduce(countAuthors, {})
+  const authorWithMostBlogs = Object.keys(authors).reduce((
+    (authorWithMaxBlogs, author) =>
+      authors[authorWithMaxBlogs] > authors[author] ? authorWithMaxBlogs : author
+  ), authors[0])
+
+  return {
+    author: authorWithMostBlogs,
+    blogs: authors[authorWithMostBlogs]
+  }
+}
+
 module.exports = {
-  dummy, totalLikes
+  dummy, totalLikes, favoriteBlog, mostBlogs
 }
 
